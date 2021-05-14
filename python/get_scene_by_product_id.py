77b857be81faf444f1892b85ed02791f33267a39
JM Delgado <delgado@ad.geoecology.uni-potsdam.de>
2021-05-14 11:01:54 +0200

    adds product ids of copernicus scenes

--- python/get_scene_by_product_id.py
+++ python/get_scene_by_product_id.py
@@ -1,7 +1,7 @@
 from sentinelsat import SentinelAPI
 import json
 
-with open('./data/credentials.json') as json_file:
+with open('/home/delgado/proj/manuscript/data/credentials.json') as json_file:
     data = json.load(json_file)
 
 api = SentinelAPI(data['username'], data['password'], 'https://scihub.copernicus.eu/dhus')
@@ -15,5 +15,15 @@ def checknget(product_id):
         print('Product {} is not online.'.format(product_id))
 
 
-product_id='12c35311-1184-4147-86fa-5fc5404cc102'
+#product_id='12c35311-1184-4147-86fa-5fc5404cc102'
+#checknget(product_id)
+
+product_id='f7a6b362-cf40-42d8-900e-a30a8060e89f'
 checknget(product_id)
+
+product_id='2da20ba3-35d4-45ac-8c8e-47ee83d4f84d'
+checknget(product_id)
+
+product_id='e0e3edf4-4f0e-471f-8aca-f9adceab1795'
+checknget(product_id)
+
